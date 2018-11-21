07a57871469e8ff304634a6b41466eba2fbcec69
Marco Minerva <marco.minerva@gmail.com>
2018-11-21 17:49:03 +0100

    Initial commit

---
+++ AlexaSkill/Extensions/ValidateRequestExtension.cs
@@ -0,0 +1,62 @@
+﻿using Alexa.NET.Request;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+
+namespace AlexaSkill.Extensions
+{
+    internal static class ValidateRequestExtension
+    {
+        public static async Task<bool> ValidateRequest(this SkillRequest skillRequest, HttpRequest request, ILogger log)
+        {
+            // Verifies that the request is indeed coming from Alexa.
+
+            request.Headers.TryGetValue("SignatureCertChainUrl", out var signatureChainUrl);
+            if (string.IsNullOrWhiteSpace(signatureChainUrl))
+            {
+                log.LogError("Validation failed. Empty SignatureCertChainUrl header");
+                return false;
+            }
+
+            Uri certUrl;
+            try
+            {
+                certUrl = new Uri(signatureChainUrl);
+            }
+            catch
+            {
+                log.LogError($"Validation failed. SignatureChainUrl not valid: {signatureChainUrl}");
+                return false;
+            }
+
+            request.Headers.TryGetValue("Signature", out var signature);
+            if (string.IsNullOrWhiteSpace(signature))
+            {
+                log.LogError("Validation failed - Empty Signature header");
+                return false;
+            }
+
+            request.Body.Position = 0;
+            var body = await request.ReadAsStringAsync();
+            request.Body.Position = 0;
+
+            if (string.IsNullOrWhiteSpace(body))
+            {
+                log.LogError("Validation failed - the JSON is empty");
+                return false;
+            }
+
+            var isTimestampValid = RequestVerification.RequestTimestampWithinTolerance(skillRequest);
+            var isValid = await RequestVerification.Verify(signature, certUrl, body);
+
+            if (!isValid && !isTimestampValid)
+            {
+                log.LogError("Validation failed - RequestVerification failed");
+            }
+
+            return isValid;
+        }
+    }
+}
---
+++ AlexaSkill/Skill.cs
@@ -0,0 +1,93 @@
+using Alexa.NET;
+using Alexa.NET.Request;
+using Alexa.NET.Request.Type;
+using Alexa.NET.Response;
+using AlexaSkill.Extensions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Threading.Tasks;
+
+namespace AlexaSkill
+{
+    public static class Skill
+    {
+        [FunctionName("SkillName")]
+        public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req, ILogger log)
+        {
+            var json = await req.ReadAsStringAsync();
+            var skillRequest = JsonConvert.DeserializeObject<SkillRequest>(json);
+
+            // Verifies that the request is indeed coming from Alexa.
+            var isValid = await skillRequest.ValidateRequest(req, log);
+            if (!isValid)
+            {
+                return new BadRequestResult();
+            }
+
+            var request = skillRequest.Request;
+            SkillResponse response = null;
+
+            try
+            {
+                if (request is LaunchRequest launchRequest)
+                {
+                    log.LogInformation("Session started");
+                    response = ResponseBuilder.Tell("Your welcome message.");
+                    response.Response.ShouldEndSession = false;
+                }
+                else if (request is IntentRequest intentRequest)
+                {
+                    // Checks whether to handle system messages defined by Amazon.
+                    var systemIntentResponse = HandleSystemIntentRequest(intentRequest);
+                    if (systemIntentResponse.IsHandled)
+                    {
+                        response = systemIntentResponse.Response;
+                    }
+                    else
+                    {
+                        // Processes request according to intentRequest.Intent.Name...
+                        response = ResponseBuilder.Tell("Your custom answer.");
+                    }
+                }
+                else if (request is SessionEndedRequest sessionEndedRequest)
+                {
+                    log.LogInformation("Session ended");
+                    response = ResponseBuilder.Empty();
+                    response.Response.ShouldEndSession = true;
+                }
+            }
+            catch
+            {
+                response = ResponseBuilder.Tell("I'm sorry, there was an unexpected error. Please, try again later.");
+            }
+
+            return new OkObjectResult(response);
+        }
+
+        private static (bool IsHandled, SkillResponse Response) HandleSystemIntentRequest(IntentRequest request)
+        {
+            SkillResponse response = null;
+
+            if (request.Intent.Name == "AMAZON.CancelIntent")
+            {
+                response = ResponseBuilder.Tell("Canceling...");
+            }
+            else if (request.Intent.Name == "AMAZON.HelpIntent")
+            {
+                response = ResponseBuilder.Tell("Help...");
+                response.Response.ShouldEndSession = false;
+            }
+            else if (request.Intent.Name == "AMAZON.StopIntent.")
+            {
+                response = ResponseBuilder.Tell("Stopping...");
+            }
+
+            return (response != null, response);
+        }
+    }
+}
