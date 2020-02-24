3a3fd758334392edcc9275c55da448a605996c4d
Marco Minerva <marco.minerva@gmail.com>
2020-02-24 18:30:09 +0100

    Create base WebAPI template project

---
+++ WebApi/AlexaSkill/Controllers/SkillController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace AlexaSkill.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SkillController : ControllerBase
+    {
+        private readonly ILogger<SkillController> logger;
+
+        public SkillController(ILogger<SkillController> logger)
+        {
+            this.logger = logger;
+        }
+    }
+}
